b0ddb059f86ea9fed35be8c86e33c28854d50c3b
CosplayCompendium <CosplayCompendium@gmail.com>
2018-05-07 16:54:24 +0200

    email veranderd

--- php/handler.php
+++ php/handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('sebastian.carofiglio@gmail.com'); // â† Your email here
+$pp->sendEmailTo('ericfastre@hotmail.com'); // â† Your email here
 
 echo $pp->process($_POST);
